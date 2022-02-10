c89f0aba93ce4876530f82a252fe2d6211fa0937
codingSlate <ananta2021k@gmail.com>
2022-02-10 11:57:19 +0530

    class component to functional component with Context Consumer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css';
 import Button from './Button';
-import   ThemeContext from './ThemeContext' 
+import ThemeContext from './ThemeContext';
 
-class App extends Component {
-  static contextType = ThemeContext
-  render() {
-    const theme = this.context;
-    return (
-      <div>
-        <div className="light">
-          <h1>Hello StackBlitz!</h1>
-          <p>Start editing to see some magic happen :) {theme} theme</p>
+const App = () => {
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <div>
+          <div className={theme}>
+            <h1>Hello StackBlitz!</h1>
+            <p>Start editing to see some magic happen :) {theme} theme</p>
+          </div>
+          <Button />
         </div>
-        <Button />
-      </div>
-    );
-  }
-}
+      )}
+    </ThemeContext.Consumer>
+  );
+};
+
+// import React, { Component } from 'react';
+// class App extends Component {
+//   static contextType = ThemeContext
+//   render() {
+//     const theme = this.context;
+//     return (
+//       <div>
+//         <div className="light">
+//           <h1>Hello StackBlitz!</h1>
+//           <p>Start editing to see some magic happen :) {theme} theme</p>
+//         </div>
+//         <Button />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
